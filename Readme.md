025b46d2de4cd71f8e948792644ffde446414d6f
Brahimi <ali.brahimi@marex.com>
2024-03-26 22:20:08 +0000

    Added longdescription

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is a port of LetsBeRational in python
--- setup.py
+++ setup.py
@@ -11,15 +11,21 @@ except ImportError:
 
 from setuptools import find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+readme = (this_directory / "README.md").read_text()
+
 setup(
     name="py_lets_be_rational",
-    version="0.0.1",
+    version="1.0.0",
     description="A port of letsBeRational",
-    author="Ali Brahimi",
+    author="Ali B.",
     license="BSD",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     cmake_install_dir="src/py_lets_be_rational",
     include_package_data=True,
-    python_requires=">=3.8"
+    python_requires=">=3.8",
+    long_description=readme,
+    long_description_content_type="text/markdown"
 )
